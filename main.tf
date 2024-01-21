02bda557118ad516eebf8b69a3b2f66ba05e9e7a
BHUPESHGCTECH <117644182+BHUPESHGCTECH@users.noreply.github.com>
2024-01-21 22:40:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-    region = "ap-south-1a"  
+    region = "ap-south-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0d980397a6e8935cd" # us-west-2
+  ami = "ami-0d980397a6e8935cd" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
   }
 }
+
