f53567b7a5311c17fc8f7569c97d14ea1818cf3c
BHUPESHGCTECH <117644182+BHUPESHGCTECH@users.noreply.github.com>
2024-01-21 22:50:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAXFYCGJHJXPNWYEFF"
-secret_key = "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"
 }
 resource "aws_instance" "foo" {
   ami = "ami-0d980397a6e8935cd"
