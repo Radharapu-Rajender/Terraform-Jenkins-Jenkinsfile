27e6aeb56af10e0bff90495a4ec38c41887ed7dc
BHUPESHGCTECH <117644182+BHUPESHGCTECH@users.noreply.github.com>
2024-01-21 22:43:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
-    region = "ap-south-1"  
-}
+region = "ap-south-1"
+access_key = "AKIAXFYCGJHJ6ATHWZ6G"
+secret_key = "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"
 
+}
 resource "aws_instance" "foo" {
-  ami = "ami-0d980397a6e8935cd" # us-west-2
+  ami = "ami-0d980397a6e8935cd"
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "bhupesh-Instance"
   }
 }
 
