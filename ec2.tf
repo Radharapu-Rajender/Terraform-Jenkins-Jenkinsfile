dd717f5d7dc83050a39b359a5ba7e8f5e818fcfc
BHUPESHGCTECH <117644182+BHUPESHGCTECH@users.noreply.github.com>
2024-01-22 16:54:23 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1" # Change this to your desired AWS region
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" # Change this to your desired AMI ID
+  instance_type = "t2.micro"
+ 
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
