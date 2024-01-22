d58482fd975ee133ff53fdf1c16d958af8aefc30
BHUPESHGCTECH <117644182+BHUPESHGCTECH@users.noreply.github.com>
2024-01-22 16:57:23 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "main" {
     Purpose = "Jenkins Demo"
   }
 }
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" # Change this to your desired AMI ID
+  instance_type = "t2.micro"
+ 
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
