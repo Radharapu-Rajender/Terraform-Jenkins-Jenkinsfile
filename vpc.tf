f5ad338d7bd062dba15bff4a0107fe970460ce0a
Radharapu-Rajender <37588111+Radharapu-Rajender@users.noreply.github.com>
2024-01-25 17:36:48 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = AKIA5QZ56GNWK7G2ZQDX
-  secret_key = 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
+  access_key = "AKIA5QZ56GNWK7G2ZQDX"
+  secret_key = "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"
   region = "ap-south-1"
 }
 
